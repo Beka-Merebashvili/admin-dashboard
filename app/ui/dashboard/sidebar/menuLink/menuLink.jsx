01bed96e6e41399a29dd10981bce79f056193590
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2023-12-06 16:56:07 +0400

    add sidebar menu

---
+++ app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -0,0 +1,19 @@
+"use client"
+
+import Link from 'next/link'
+import styles from './menuLink.module.css'
+ 
+
+const MenuLink = ({item}) => {
+
+  
+
+  return (
+    <Link href={item.path} className={styles.container}>
+      {item.icon}
+      {item.title}
+    </Link>
+  )
+}
+
+export default MenuLink
