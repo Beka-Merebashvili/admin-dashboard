cbdfb2deb93de583ca6ece8ac90be2e0ce439cb4
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2023-12-06 22:15:27 +0400

    finish card

---
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -0,0 +1,11 @@
+import styles from "./rightbar.module.css"
+
+const Rightbar = () => {
+    return (
+        <div>
+            rightbar
+        </div>
+    )
+}
+
+export default Rightbar
---
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -0,0 +1,10 @@
+import styles from "./transactions.module.css"
+const Transactions = () => {
+    return (
+        <div>
+            transactionss
+        </div>
+    )
+}
+
+export default Transactions
