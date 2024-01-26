3c65875593c5571832acafe5389b7bcd662b762c
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-01-26 23:52:39 +0400

    fix login form

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -5,6 +5,7 @@ import { User, Product } from "./models";
 import { connectToDb } from "./utils";
 import { redirect } from "next/navigation";
 import bcrypt from "bcrypt";
+import { signIn } from "../auth";
 
 export const addUser = async (formData) => {
   const { username, email, password, phone, address, isAdmin, isActive } =
@@ -152,4 +153,18 @@ export const deleteProduct = async (formData) => {
   }
 
   revalidatePath("/dashboard/products");
+};
+
+
+export const authenticate = async (prevState, formData) => {
+  const { username, password } = Object.fromEntries(formData);
+
+  try {
+    await signIn("credentials", { username, password });
+  } catch (err) {
+    if (err.message.includes("CredentialsSignin")) {
+      return "Wrong Credentials";
+    }
+    throw err;
+  }
 };
